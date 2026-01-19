c238e414939ed75a32e6ad6dd114f97af106b17c
Marián Černý <marian@pematon.com>
2026-01-19 19:10:25 +0100

    Install for local user
    Install to ~/Library/Keyboard Layouts instead of system-wide /Library/Keyboard Layouts.
    There is no longer much need for a system-wide install, as the last built-in system layout can no longer be removed from System Settings.

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ cd $BASEDIR
 
 set -ex
 
-sudo cp -R U.S.-Czech.bundle/ /Library/Keyboard\ Layouts/U.S.-Czech.bundle/
-sudo touch /Library/Keyboard\ Layouts
+cp -R U.S.-Czech.bundle/ ~/Library/Keyboard\ Layouts/U.S.-Czech.bundle/
+touch ~/Library/Keyboard\ Layouts
 
 { set +x; } 2>/dev/null
 
 echo ""
-echo "  Now go to System Preferences > Keyboard > Input Sources and add the"
-echo "  U.S. - Czech layout. You can then switch to the new layout in Input menu"
-echo "  in the menu bar (the flag icon)."
+echo "  Now go to System Settings > Keyboard > Input Sources > Edit... and add the"
+echo "  Czech > U.S. - Czech layout. You can then switch to the new layout in Input"
+echo "  menu in the menu bar."
 echo ""
 echo "  If this is your first install, the new layout should work right away."
 echo "  If you are updating the layout, you need to log out or restart the computer."
